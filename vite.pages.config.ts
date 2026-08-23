67296cc42955b414e8dbce86d35e927877a2fa25
KeysWithDon <keyswithdon@gmail.com>
2026-08-23 07:46:04 -0400

    Use Faithful Keys Pages path

--- vite.pages.config.ts
+++ vite.pages.config.ts
@@ -1,4 +1,4 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig({ base: "/cadence/", plugins: [react()] });
+export default defineConfig({ base: "/faithful-keys/", plugins: [react()] });
